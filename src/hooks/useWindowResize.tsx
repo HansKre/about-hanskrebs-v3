16dbda9496683acfe6aba402cf127e73701ef307
hans <hans.krebs@me.com>
2021-09-05 10:05:22 +0200

    Implement custom hook to retrieve current window size.

---
+++ src/hooks/useWindowResize.tsx
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react';
+
+/**
+ * 
+ * @returns current window width and height
+ */
+export default function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    } as {width: number | undefined; height: number | undefined});
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        window.addEventListener("resize", handleResize);
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    return windowSize;
+}
