5c1314865dc835dba63b68eaca7c485afc80797f
hans <hans.krebs@me.com>
2021-09-07 08:15:31 +0200

    Add frontend works.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import CustomTheme from './styles/Theme';
 import LandingPage from './layouts/LandingPage';
 import About from './layouts/About';
 import Contact from './layouts/Contact';
+import FrontendWorks from './layouts/FrontendWorks';
 
 function App() {
   return (
     <ThemeProvider theme={CustomTheme}>
       <LandingPage />
       <About />
+      <FrontendWorks />
       <Contact />
     </ThemeProvider>
   );
--- src/components/ProgressiveImg.tsx
+++ src/components/ProgressiveImg.tsx
@@ -1,28 +1,32 @@
-import ProgressiveImage from 'react-progressive-image'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable @typescript-eslint/no-shadow */
+import ProgressiveImage from 'react-progressive-image';
 import LoadingAnim from './loading-anim/LoadingAnim';
 
 type Props = {
-    src: string;
-    placeholder: string;
-    className?: string;
-    animShift?: {
-        top: string;
-        left: string;
-    }
-}
+  src: string;
+  placeholder: string;
+  className?: string;
+  animShift?: {
+    top: string;
+    left: string;
+  };
+};
 export default function ProgressiveImg(props: Props) {
-    const {src, placeholder, className, animShift} = props;
-    return (
-        <div style={{position: 'relative', display: 'flex', alignItems: 'center'}}>
-            <ProgressiveImage src={src} placeholder={placeholder}>
-                {(src: string | undefined, loading: boolean) => (
-                    <>
-                        {loading ? <LoadingAnim absolute {...animShift} /> : ''}
-                        <img src={src} alt={src} className={className} />
-                    </>
-                )
-                }
-            </ProgressiveImage>
-        </div>
-    )
+  // eslint-disable-next-line object-curly-newline
+  const { src, placeholder, className, animShift } = props;
+  return (
+    <div
+      style={{ position: 'relative', display: 'flex', alignItems: 'center' }}
+    >
+      <ProgressiveImage src={src} placeholder={placeholder}>
+        {(src: string | undefined, loading: boolean) => (
+          <>
+            {loading ? <LoadingAnim absolute {...animShift} /> : ''}
+            <img src={src} alt={src} className={className} />
+          </>
+        )}
+      </ProgressiveImage>
+    </div>
+  );
 }
