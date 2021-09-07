c1aa02a401ec169e1bc9f6d1c4943341a0858ddd
hans <hans.krebs@me.com>
2021-09-07 18:37:56 +0200

    Improve Contact-layout styling.

--- src/layouts/Contact.tsx
+++ src/layouts/Contact.tsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import { useTheme } from '@material-ui/core';
 import { SocialIcon } from 'react-social-icons';
 import { hoverable } from '../styles/Styles';
+import useWindowSize from '../hooks/useWindowResize';
 
 const useStyles = makeStyles({
   hoverableSocial: {
@@ -11,19 +12,24 @@ const useStyles = makeStyles({
       transform: 'scale(1.15)',
     },
   },
-  button: {
-    backgroundColor: '#2d2c30',
+  hoverableLink: {
+    ...hoverable,
+    backgroundColor: 'Transparent',
     border: 'none',
-    borderRadius: '10px',
-    color: 'white',
-    width: '100%',
     cursor: 'pointer',
+    outline: 'none',
+    alignSelf: 'flex-start',
+    '&:hover': {
+      transform: 'scale(1.10)',
+    },
   },
 });
 
 export default function Contact() {
   const theme = useTheme();
-  const classes = useStyles();
+  const { width: windowWidth } = useWindowSize();
+  const upMd = windowWidth >= theme.breakpoints.values.md;
+  const classes = useStyles({ upMd });
   return (
     <div
       style={{
@@ -31,24 +37,33 @@ export default function Contact() {
         flexDirection: 'column',
         justifyContent: 'center',
         height: '100%',
-        padding: '7.5vw',
+        padding: upMd ? '7.5vw 15vw' : '7.5vw',
         backgroundColor: theme.palette.primary.main,
       }}
     >
       <h1 id='contact'>Let’s Build Something Together 👋</h1>
       <Grid container>
         <Grid item container direction='column' xs={12}>
-          <h3
-            style={{
-              paddingTop: 0,
-              paddingBottom: '40px',
-            }}
-          >
+          <h3 style={{ paddingTop: 0 }}>
             Feel free to reach me out if you’re looking for a developer, have a
             question, or just want to connect.
           </h3>
-          {/* TODO mailto-link */}
-          <h3>hans@hanskrebs.me</h3>
+          <button type='button' className={`${classes.hoverableLink}`}>
+            <h3>
+              <a
+                style={{
+                  fontSize: 'inherit',
+                  textDecoration: 'underline',
+                  textDecorationThickness: '2px',
+                  textUnderlineOffset: '3px',
+                  textDecorationColor: theme.palette.secondary.main,
+                }}
+                href='mailto:hans@hanskrebs.me'
+              >
+                hans@hanskrebs.me
+              </a>
+            </h3>
+          </button>
         </Grid>
         <Grid
           item
@@ -69,21 +84,22 @@ export default function Contact() {
               }}
             >
               <SocialIcon
-                style={{ height: '2.4em', width: '2.4em' }}
+                // style={{ height: '2.4em', width: '2.4em' }}
+                bgColor={theme.palette.secondary.light}
                 className={`${classes.hoverableSocial}`}
                 url='https://github.com/hanskre'
                 target='_blank'
                 rel='noopener noreferrer'
               />
               <SocialIcon
-                style={{ height: '2.4em', width: '2.4em' }}
+                // style={{ height: '2.4em', width: '2.4em' }}
                 className={`${classes.hoverableSocial}`}
                 url='https://www.linkedin.com/in/hans-krebs-63b35034/'
                 target='_blank'
                 rel='noopener noreferrer'
               />
               <SocialIcon
-                style={{ height: '2.4em', width: '2.4em' }}
+                // style={{ height: '2.4em', width: '2.4em' }}
                 className={`${classes.hoverableSocial}`}
                 url='https://www.instagram.com/hanso711/'
                 target='_blank'
