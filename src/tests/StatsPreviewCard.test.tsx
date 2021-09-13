83a16bbe65e1ab70730f0a2bdbdef2f5cbec42e1
hans <hans.krebs@me.com>
2021-09-13 08:47:05 +0200

    Silence linting issues.

--- src/tests/StatsPreviewCard.test.tsx
+++ src/tests/StatsPreviewCard.test.tsx
@@ -1,11 +1,18 @@
+/* eslint-disable */
 import { render, screen } from '@testing-library/react';
 import MarginsLeftRight from '../components/MarginsLeftRight';
 
 describe('<StatsPreviewCard />', () => {
   it('should render header text', () => {
-    render(<MarginsLeftRight><h1>foo</h1></MarginsLeftRight>);
+    render(
+      <MarginsLeftRight>
+        <h1>foo</h1>
+      </MarginsLeftRight>
+    );
     expect(screen.getByText(/Get/i)).toBeInTheDocument();
     expect(screen.getByText(/insights/i)).toBeInTheDocument();
-    expect(screen.getByText(/that help your business grow./i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/that help your business grow./i)
+    ).toBeInTheDocument();
   });
 });
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,7 @@
 import { CreateCSSProperties, PropsFunc } from '@material-ui/styles/withStyles';
 
 export type CSSStyle =
+  // eslint-disable-next-line no-undef
   | React.CSSProperties
   | CreateCSSProperties<{}>
   | PropsFunc<{}, CreateCSSProperties<{}>>;
